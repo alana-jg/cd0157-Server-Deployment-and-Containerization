2c60a15c211b524e2320a20d4b91d02dd69dd69e
alana-jg <alana_graham@hotmail.co.uk>
2023-04-26 22:32:09 +0100

    changes to test pipeline

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy1")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
